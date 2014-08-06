62e35eabd420135759a9f7664db4c3d7b0895668
Guinsly Mondésir <guinslym@gmail.com>
2014-08-06 09:30:29 -0400

    final version

--- spec/course.rb
+++ spec/course.rb
@@ -80,26 +80,36 @@ class ProgramCourse
 end#end class
 
 #HOW TO CALL THIS PROGRAM
+
+=begin
 classes = ProgramCourse.new("http://www.etudesup.uottawa.ca/Default.aspx?tabid=1726&monControl=Programmes&ProgId=985")
 classes = classes.all_courses
 
 total = 0
-classes.each { |n| total += 1 if n.prerequis.eql? false }
+n_prerequis_course = []
+prerequis_course = []
+classes.each do |n| 
+  if n.prerequis.eql? false
+    total += 1 
+    n_prerequis_course << [n.code, n.title]
+  else #course with a prerequisite
+    prerequis_course << [n.code, n.title]
+  end
+end
 puts "size = #{classes.size} and total #{total}"
-
+#puts n_prerequis_course
 
 
 
 puts AsciiCharts::Cartesian.new([["without-prerequisite", total],["prerequise", classes.size-total] ], bar: true, :title => 'nb Courses with/without prerequisites').draw
 
-table = Terminal::Table.new do |t|
-  t << ['One', 1]
-  t << :separator
-  t.add_row ["Two\nDouble", 2]
-  t.add_separator
-  t.add_row ['Three', 3]
+table = Terminal::Table.new :title => "Course without prerequisites",:headings => ['Courses Code', 'Course Title'] do |t|
+  n_prerequis_course.each do |course|
+    t.add_row [course.first, course.last]
+  end
 end
-table.align_column(1, :right)
-table << ["Four", {:value => 4.0, :alignment => :center}]
+table.align_column(1, :left)
+
 puts table
-#TODO how to make this programm more like a Service Oriented object
+
+=end
