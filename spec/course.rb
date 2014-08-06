2dd3de7cffb8c7819ce59c65dbcc413e1dc66e4f
Guinsly Mondésir <guinslym@gmail.com>
2014-08-06 00:06:31 -0400

    adding a bit of graphic

--- spec/course.rb
+++ spec/course.rb
@@ -1,5 +1,7 @@
 require 'open-uri'
 require 'nokogiri'
+require 'ascii_charts'
+require 'terminal-table'
 
 class Lecture
   attr_accessor :code, :title, :description, :prerequis, :credit
@@ -61,8 +63,9 @@ class ProgramCourse
   def all_courses
       find_courses = []
       if @divs_found
-        divs_found[3..75].each do |courses|
-          if courses.children[0]
+        #I need to refactor [3..75] I'm not supposed to put number
+        divs_found.each do |courses|
+          if courses.children[0] && courses.children[0].content.split.first.match(/\d{4}/)
             c = Lecture.new(courses)
             find_courses << c
 
@@ -76,11 +79,27 @@ class ProgramCourse
 
 end#end class
 
+#HOW TO CALL THIS PROGRAM
 classes = ProgramCourse.new("http://www.etudesup.uottawa.ca/Default.aspx?tabid=1726&monControl=Programmes&ProgId=985")
 classes = classes.all_courses
 
-classes.each { |n| puts n.title }
+total = 0
+classes.each { |n| total += 1 if n.prerequis.eql? false }
+puts "size = #{classes.size} and total #{total}"
 
 
+
+
+puts AsciiCharts::Cartesian.new([["without-prerequisite", total],["prerequise", classes.size-total] ], bar: true, :title => 'nb Courses with/without prerequisites').draw
+
+table = Terminal::Table.new do |t|
+  t << ['One', 1]
+  t << :separator
+  t.add_row ["Two\nDouble", 2]
+  t.add_separator
+  t.add_row ['Three', 3]
+end
+table.align_column(1, :right)
+table << ["Four", {:value => 4.0, :alignment => :center}]
+puts table
 #TODO how to make this programm more like a Service Oriented object
-#TODO how to test the class... cause it have a method??? preparation in this programm
--- spec/lib/course_spec.rb
+++ spec/lib/course_spec.rb
@@ -5,10 +5,26 @@ require 'nokogiri'
 LINK = "http://www.etudesup.uottawa.ca/Default.aspx?tabid=1726&monControl=Programmes&ProgId=985"
 
 RSpec.describe "ProgramCourse" do
+  it "should be an instance of ProgramCourse" do
+    classes = ProgramCourse.new(LINK)
+    #expect(classes.class).to be_a(ProgramCourse)
+  end
   it "should have many divs" do
     classes = ProgramCourse.new(LINK)
     classes = classes.all_courses
     expect(classes.size).to  be >= 5
     #expect(course.data.size).to eq(20)
   end
+  it "should raise error without a link" do
+    #
+  end
+  it ".code should contains 4 number" do
+    classes = ProgramCourse.new(LINK)
+    classes = classes.all_courses
+    expect(classes.first.code).to match(/\d{4}/)
+  end
+end
+
+RSpec.describe "Lecture" do
+
 end
