419cf5138ed21c81340ed758238b718a305ed688
Guinsly Mondésir <guinslym@gmail.com>
2014-08-05 23:06:43 -0400

    starting rspec

--- spec/lib/course_spec.rb
+++ spec/lib/course_spec.rb
@@ -2,11 +2,13 @@ require_relative "../spec_helper"
 require_relative "../course"
 require 'open-uri'
 require 'nokogiri'
+LINK = "http://www.etudesup.uottawa.ca/Default.aspx?tabid=1726&monControl=Programmes&ProgId=985"
 
-RSpec.describe "Course" do
-  it "should contains data" do
-    course = Course.new
-    expect(course.data).to be_truthy
-    expect(course.data.size).to eq(20)
+RSpec.describe "ProgramCourse" do
+  it "should have many divs" do
+    classes = ProgramCourse.new(LINK)
+    classes = classes.all_courses
+    expect(classes.size).to  be >= 5
+    #expect(course.data.size).to eq(20)
   end
 end
