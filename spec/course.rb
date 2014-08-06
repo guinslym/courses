23a9031d3d66ec7641f4af102eb42c00393db201
Guinsly Mondésir <guinslym@gmail.com>
2014-08-06 09:36:53 -0400

    add init.rb

--- spec/course.rb
+++ spec/course.rb
@@ -84,32 +84,5 @@ end#end class
 =begin
 classes = ProgramCourse.new("http://www.etudesup.uottawa.ca/Default.aspx?tabid=1726&monControl=Programmes&ProgId=985")
 classes = classes.all_courses
-
-total = 0
-n_prerequis_course = []
-prerequis_course = []
-classes.each do |n| 
-  if n.prerequis.eql? false
-    total += 1 
-    n_prerequis_course << [n.code, n.title]
-  else #course with a prerequisite
-    prerequis_course << [n.code, n.title]
-  end
-end
-puts "size = #{classes.size} and total #{total}"
-#puts n_prerequis_course
-
-
-
-puts AsciiCharts::Cartesian.new([["without-prerequisite", total],["prerequise", classes.size-total] ], bar: true, :title => 'nb Courses with/without prerequisites').draw
-
-table = Terminal::Table.new :title => "Course without prerequisites",:headings => ['Courses Code', 'Course Title'] do |t|
-  n_prerequis_course.each do |course|
-    t.add_row [course.first, course.last]
-  end
-end
-table.align_column(1, :left)
-
-puts table
-
+##and start doing stuff...
 =end
